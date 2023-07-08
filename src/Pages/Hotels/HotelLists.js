57dac4f453ebc6e01039b3449eed2f3414f12b70
jfmemon <jannatul15-2497@diu.edu.bd>
2023-07-08 22:01:08 +0600

    hotelList.js file updated with frontend

--- src/Pages/Hotels/HotelLists.js
+++ src/Pages/Hotels/HotelLists.js
@@ -3,15 +3,13 @@ import React from 'react';
 const HotelLists = ({ hotel }) => {
     const { id, img, title, details, price } = hotel;
     return (
-        <div className="h-60 card card-side bg-base-100 shadow-xl m-5 p-5" >
-            <img className='p-2' src={img} alt="" />
-            <div className="card-body">
-                <h2 className="card-title">{title}</h2>
-                <p>{details}</p>
-                <h3 className='text-1xl font-semibold'>Price: {price} tk <span>(Per night)</span></h3>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-outline btn-warning">Book Now</button>
-                </div>
+        <div className="flex flex-col p-5 my-4 bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 ms-auto me-auto">
+            <img className="rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={img} alt="" />
+            <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{details}</p>
+                <p className="mb-3 font-semibold text-gray-700 dark:text-gray-400">{price} tk (Per night)</p>
+                <button className='btn btn-outline btn-warning w-28'>Book Now</button>
             </div>
         </div>
     );
